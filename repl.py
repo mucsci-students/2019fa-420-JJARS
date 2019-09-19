ff9c48f576763d0d36b4866aa4535b4f6f65b499
Devon M Sangrey <dmsangre@shortliffe.muad.local>
2019-09-19 15:01:21 -0400

    updated add and remove commands

--- repl.py
+++ repl.py
@@ -8,20 +8,27 @@ class MyPrompt(Cmd):
   def do_exit(self, inp):
     print ("Exited")
     return True
-
+  
+  def do_create_diagram:
+    """Creates a UML diagram to edit"""
+    #diagram = UMLDiagram()
   def do_add_class(self, inp):
     """add_class [class name]                                                   
-Adds new class if one with that name does not already exist"""
+Adds new class if one with that name does not already exist"""  
+    #add_class_name = diagram.add_class(inp)
     print("Added '{}' class".format(inp))
   
   def do_remove_class(self, inp):
     """remove_class [class name]                                                
 Removes class if it exists"""
+    #remove_class_name = diagram.remove_class(inp)
     print("Removed '{}' class".format(inp))
   
   def do_list_classes(self,inp):
     """Lists all classes"""
     print("Listing all classes:\n")
+    #class_names = diagram.get_all_class_names()
+    #print('\n'.join(class_names))
   
   def do_rename_class(self, inp):
     """rename_class [class name] [new class name]                               
