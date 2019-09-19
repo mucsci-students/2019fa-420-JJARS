614e1a4cfb0d40780886c2fbca9e2b874224372b
Jessie Hildebrandt <jessieh@jessieh.net>
2019-09-19 15:04:26 -0400

    Fix typing errors

--- scruml/uml_diagram.py
+++ scruml/uml_diagram.py
@@ -2,27 +2,29 @@
 # uml_diagram.py
 # Team JJARS
 
+from typing import Optional, Dict, List
+
 class UMLDiagram:
 
-    def __init__() -> None:
-        self.__class_list: Dict[str, Dict[str, str]] = dict()
-        self.__relationship_list: Dict[Frozenset[str], Dict[str, Dict[str, str]]] = dict()
+    def __init__(self) -> None:
+        self.__classes: Dict[str, Dict[str, str]] = dict()
+        self.__relationships: Dict[Frozenset[str], Dict[str, Dict[str, str]]] = dict()
 
     def add_class(class_name: str) -> Optional[str]:
-        if class_name in self.__class_list:
+        if class_name in self.__classes:
             return None
-        self.__class_list[class_name] = dict()
+        self.__classes[class_name] = dict()
 
     def remove_class(class_name: str) -> Optional[str]:
-        if class_name not in self.__class_list:
+        if class_name not in self.__classes:
             return None
-        del self.__class_list[class_name]
+        del self.__classes[class_name]
 
     def get_all_class_names() -> List[str]:
-        return list(self.__class_list.keys())
+        return list(self.__classes.keys())
 
     def rename_class(old_class_name: str, new_class_name: str) -> Optional[str]:
-        if old_class_name not in self.__class_list or new_class_name in self.__class_list:
+        if old_class_name not in self.__classes or new_class_name in self.__classes:
             return None
-        self.__class_list[new_class_name] = self.__class_list.pop(old_class_name)
+        self.__classes[new_class_name] = self.__classes.pop(old_class_name)
         return new_class_name
