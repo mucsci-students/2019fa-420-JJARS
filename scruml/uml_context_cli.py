52c3d09e35c06f478978333a2f552f1d489f8d0f
Jessie Hildebrandt <jessieh@jessieh.net>
2019-09-21 01:22:53 -0400

    Add placeholder CLI context

---
+++ scruml/uml_context_cli.py
@@ -0,0 +1,11 @@
+# ScrUML
+# uml_context_cli.py
+# Team JJARS
+
+
+def activate() -> None:
+    """Activate the CLI application context."""
+    print("PLACEHOLDER: uml_context_cli.py is currently a placeholder file.")
+    print("             ScrUML doesn't do anything yet!")
+    print("             ....Devon and Ryan will fix that, though.")
+    pass
