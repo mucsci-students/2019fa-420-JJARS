8c7d19e265aeb783a3794468d6e22f2243937250
Chris Jones <MasterC@Windsors-MacBook-Pro.local>
2019-09-24 17:12:47 -0400

    Updated save and load functions: import error

--- scruml/uml_filesystem_io.py
+++ scruml/uml_filesystem_io.py
@@ -3,21 +3,26 @@
 #Team JJARS
 
 import yaml
-#TODO: Fix problem with import statement
+#import not fixed yet
 from scruml.uml_diagram import UMLDiagram
 
-def uml_filesystem_io_save( diagram: UMLDiagram,file_path: str) -> bool:
+def uml_filesystem_io_save( diagram: UMLDiagram, file_path: str) -> bool:
 		with open(file_path,'wb') as f:
-			if True:
+			if f:
 				yaml.dump(diagram, f, protocol=-1)
 				return True
 			else:
 				return False
 
-def uml_filesystem_io_load( diagram: UMLDiagram,file_path: str ) -> bool:
+def uml_filesystem_io_load( diagram: UMLDiagram, file_path: str) -> bool:
 		is_open = open(file_path, 'rb')
-		diagram = yaml.load( is_open)
-		if UMLDiagram is {}:
+		diagram = yaml.load(is_open)
+		if diagram is {}:
 			return False
 		else:
 			return True
+
+
+	
+
+
