92d463d61b1eab69da631c36e6e167a5159279d9
Chris Jones <MasterC@Windsors-MacBook-Pro.local>
2019-09-24 18:17:53 -0400

    Updated Save and Load functions

--- scruml/uml_filesystem_io.py
+++ scruml/uml_filesystem_io.py
@@ -4,23 +4,21 @@
 
 import yaml
 #import not fixed yet
-from scruml.uml_diagram import UMLDiagram
+from uml_diagram import UMLDiagram
 
 def uml_filesystem_io_save( diagram: UMLDiagram, file_path: str) -> bool:
 		with open(file_path,'wb') as f:
 			if f:
 				yaml.dump(diagram, f, protocol=-1)
 				return True
-			else:
-				return False
+			return False
 
 def uml_filesystem_io_load( diagram: UMLDiagram, file_path: str) -> bool:
 		is_open = open(file_path, 'rb')
 		diagram = yaml.load(is_open)
 		if diagram is {}:
 			return False
-		else:
-			return True
+		return True
 
 
 	
