747f7d3097e96ab8a17b3878e01e4f29ac8f7054
Devon Sangrey <dmsangre@millersville.edu>
2019-09-24 20:59:32 -0400

    Implemented CLI using uml_diagram API

--- scruml/__init__.py
+++ scruml/__init__.py
@@ -1,14 +1,14 @@
 # ScrUML
 # __init__.py
 # Team JJARS
-# ----------
-# main
+from scruml.uml_context_cli import uml_context_cli
+
+
 def main() -> None:
     """Act as primary entry point for the program."""
-    print("Hello, world!")
+    uml_context_cli.activate()
 
 
-# ----------
 # If this file was loaded explicitly, run main()
 if __name__ == "__main__":
     main()
--- scruml/uml_context_cli.py
+++ scruml/uml_context_cli.py
@@ -1,39 +1,46 @@
 #ScruML                                                                         
-#repl.py                                                                        
+#uml_context_cli.py                                                                       
 #Team JJARS                                                                     
 
-from cmd import Cmd
+import cmd
+from scruml.uml_diagram import UMLDiagram
+umld: UMLDiagram = UMLDiagram()
 
-class MyPrompt(Cmd):
+class uml_context_cli(cmd.Cmd):
+
   def do_exit(self, inp):
     print ("Exited")
     return True
-  
-  def do_create_diagram:
-    """Creates a UML diagram to edit"""
-    #diagram = UMLDiagram()
+
   def do_add_class(self, inp):
     """add_class [class name]                                                   
 Adds new class if one with that name does not already exist"""  
-    #add_class_name = diagram.add_class(inp)
-    print("Added '{}' class".format(inp))
+    add_class_name: str = umld.add_class(inp)
+    if add_class_name == None:
+      print ("'{}' class already exists".format(inp))
+    else:
+      print("Added '{}' class".format(inp))
   
   def do_remove_class(self, inp):
     """remove_class [class name]                                                
 Removes class if it exists"""
-    #remove_class_name = diagram.remove_class(inp)
-    print("Removed '{}' class".format(inp))
+    remove_class_name: str = umld.remove_class(inp)
+    if remove_class_name == None:
+      print ("'{}' class does not exist".format(inp))
+    else:
+      print("Removed '{}' class".format(inp))
   
   def do_list_classes(self,inp):
     """Lists all classes"""
-    print("Listing all classes:\n")
-    #class_names = diagram.get_all_class_names()
-    #print('\n'.join(class_names))
+    print("Listing all classes:")
+    class_names: List[str] = umld.get_all_class_names()
+    print('\n'.join(class_names))
   
   def do_rename_class(self, inp):
     """rename_class [class name] [new class name]                               
 Changes name of class if one of that name exists"""
-    names = inp.split()
+    names: str = inp.split()
+    umld.rename_class(names[0], names[1])
     print("changing "+names[0]+" to "+names[1])
   
   def do_save(self, inp):
@@ -42,8 +49,7 @@ Changes name of class if one of that name exists"""
   def do_load(self, inp):
     """load [filename]
 Loads existing UML diagram"""
-    print ("Loading '{}'".format(inp))                                                           
-
-MyPrompt().cmdloop()
-
-
+    print ("Loading '{}'".format(inp))   
+                                                      
+  def activate():
+    uml_context_cli().cmdloop()
