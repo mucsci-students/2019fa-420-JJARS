80d636c8025cc86d3b215c398590d7258038c7e0
Jessie Hildebrandt <jessieh@jessieh.net>
2019-09-26 10:13:16 -0400

    Refactor CLI context module

--- scruml/uml_context_cli.py
+++ scruml/uml_context_cli.py
@@ -1,55 +1,59 @@
-#ScruML                                                                         
-#uml_context_cli.py                                                                       
-#Team JJARS                                                                     
-
+# ScruML
+# uml_context_cli.py
+# Team JJARS
 import cmd
-from scruml.uml_diagram import UMLDiagram
-umld: UMLDiagram = UMLDiagram()
 
-class uml_context_cli(cmd.Cmd):
+import uml_filesystem_io
+from uml_diagram import UMLDiagram
 
-  def do_exit(self, inp):
-    print ("Exited")
-    return True
 
-  def do_add_class(self, inp):
-    """add_class [class name]                                                   
-Adds new class if one with that name does not already exist"""  
-    add_class_name: str = umld.add_class(inp)
-    if add_class_name == None:
-      print ("'{}' class already exists".format(inp))
-    else:
-      print("Added '{}' class".format(inp))
-  
-  def do_remove_class(self, inp):
-    """remove_class [class name]                                                
-Removes class if it exists"""
-    remove_class_name: str = umld.remove_class(inp)
-    if remove_class_name == None:
-      print ("'{}' class does not exist".format(inp))
-    else:
-      print("Removed '{}' class".format(inp))
-  
-  def do_list_classes(self,inp):
-    """Lists all classes"""
-    print("Listing all classes:")
-    class_names: List[str] = umld.get_all_class_names()
-    print('\n'.join(class_names))
-  
-  def do_rename_class(self, inp):
-    """rename_class [class name] [new class name]                               
-Changes name of class if one of that name exists"""
-    names: str = inp.split()
-    umld.rename_class(names[0], names[1])
-    print("changing "+names[0]+" to "+names[1])
-  
-  def do_save(self, inp):
-    """Saves UML diagram"""
-  
-  def do_load(self, inp):
-    """load [filename]
-Loads existing UML diagram"""
-    print ("Loading '{}'".format(inp))   
-                                                      
-  def activate():
-    uml_context_cli().cmdloop()
+class __UMLShell(cmd.Cmd):
+
+    __diagram: UMLDiagram = UMLDiagram()
+
+    def do_exit(self, arg: str):
+        print("Exited")
+        return True
+
+    def do_add_class(self, arg: str):
+        """add_class [class name]
+      Adds new class if one with that name does not already exist"""
+        add_class_name: str = self.__diagram.add_class(arg)
+        if add_class_name == None:
+            print("'{}' class already exists".format(arg))
+        else:
+            print("Added '{}' class".format(arg))
+
+    def do_remove_class(self, arg: str):
+        """remove_class [class name]
+      Removes class if it exists"""
+        remove_class_name: str = __diagram.remove_class(arg)
+        if remove_class_name == None:
+            print("'{}' class does not exist".format(arg))
+        else:
+            print("Removed '{}' class".format(arg))
+
+    def do_list_classes(self, arg: str):
+        """Lists all classes"""
+        print("Listing all classes:")
+        class_names: List[str] = __diagram.get_all_class_names()
+        print("\n".join(class_names))
+
+    def do_rename_class(self, arg: str):
+        """rename_class [class name] [new class name]
+      Changes name of class if one of that name exists"""
+        names: str = arg.split()
+        __diagram.rename_class(names[0], names[1])
+        print("changing " + names[0] + " to " + names[1])
+
+    def do_save(self, arg: str):
+        """Saves UML diagram"""
+
+    def do_load(self, arg: str):
+        """load [filename]
+      Loads existing UML diagram"""
+        print("Loading '{}'".format(arg))
+
+
+def activate():
+    __UMLShell().cmdloop()
