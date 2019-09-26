fc3a6f6eced3dc5889d1bfb315d819c2d7d3f86e
Jessie Hildebrandt <jessieh@jessieh.net>
2019-09-26 10:12:41 -0400

    Add dummy filesystem module

---
+++ scruml/uml_filesystem_io.py
@@ -0,0 +1,12 @@
+# ScrUML
+# uml_filesystem_io
+# Team JJARS
+from uml_diagram import UMLDiagram
+
+# Dummy file
+def save_diagram(diagram: UMLDiagram, file_path: str):
+    pass
+
+
+def load_diagram(diagram: UMLDiagram, file_path: str):
+    pass
