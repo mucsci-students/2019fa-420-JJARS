3527fdf42236cf095fc3ec11ff981e09b7779c00
Jessie Hildebrandt <jessieh@jessieh.net>
2019-09-27 18:58:40 -0400

    Begin partial restructuring of CLI context

--- scruml/uml_context_cli.py
+++ scruml/uml_context_cli.py
@@ -3,6 +3,8 @@
 # Team JJARS
 import cmd
 from typing import List
+from typing import Optional
+from typing import Tuple
 
 from scruml import uml_filesystem_io
 from scruml.uml_diagram import UMLDiagram
@@ -15,23 +17,59 @@ class __UMLShell(cmd.Cmd):
     prompt: str = "ScrUML> "
     __diagram: UMLDiagram = UMLDiagram()
 
-    def do_exit(self, arg: str) -> bool:
-        """Usage: exit
-Exits ScrUML"""
-        print("Thank you for using ScrUML. Goodbye!")
-        return True
+    # ----------
+    # Helper functions
 
-    def do_add_class(self, arg: str) -> None:
-        """Usage: add_class <class name>
-Adds new class if one with that name does not already exist"""
+    def __classify_identifier(self, ident: str) -> str:
+        ident = ident.strip()
+
+        if ident.startswith("[") and ident.endswith("]"):
+            ident = ident[1:-1]
+            # TODO, parse rest of relationship identifier: [ classA, classB, name ]
+        # TODO, parse rest of class identifier: name
+        # no spaces in class names,
+        # no spaces in relationship names
+        return ""
+
+    def __parse_class_identifier(self, ident: str) -> str:
+        return ""
+
+    def __parse_relationship_identifier(
+        self, ident: str
+    ) -> Tuple[str, str, Optional[str]]:
+        return ("", "", None)
+
+    # ----------
+    # "Add" functions
+
+    def do_add(self, arg: str) -> None:
+        """Usage: add <identifier>
+Adds new class or relationship if one with that identifier does not already exist
+For help with identifiers, type in 'help identifiers'"""
+        # TODO, implement identifier classification, parsing, and dispatching to subcommand
+        pass
+
+    def __add_class(self, arg: str) -> None:
+        """Adds new class if one with that name does not already exist"""
         if not self.__diagram.add_class(arg):
             print("Class '{}' already exists in the diagram".format(arg))
         else:
             print("Added class '{}'".format(arg))
 
+    def __add_relationship(self, arg: str) -> None:
+        """Adds new relationship if one with that identifier does not already exist"""
+        pass
+
+    # ----------
+    # "Remove" functions
+
+    def do_remove(self, arg: str) -> None:
+        """Usage: remove <identifier>"""
+        # TODO, implement identifier classification, parsing, and dispatching to subcommand
+        pass
+
     def do_remove_class(self, arg: str) -> None:
-        """Usage: remove_class <class name>
-Removes class if it exists"""
+        """Removes class if it exists"""
         if not self.__diagram.remove_class(arg):
             print("Class '{}' does not exist in the diagram".format(arg))
         else:
@@ -73,6 +111,12 @@ Saves the current UML diagram to a file"""
 Loads an existing UML diagram from a file"""
         print("Loading diagram from '{}'".format(arg))
         self.__diagram = uml_filesystem_io.load_diagram(arg)
+
+    def do_exit(self, arg: str) -> bool:
+        """Usage: exit
+Exits ScrUML"""
+        print("Thank you for using ScrUML. Goodbye!")
+        return True
 
 
 def activate() -> None:
