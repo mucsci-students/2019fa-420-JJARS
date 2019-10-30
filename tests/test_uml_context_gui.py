d913f5f5505027dc61d7d3f54558b455df68bd4e
Chris Jones <MasterC@Windsors-MacBook-Pro.local>
2019-10-30 09:04:34 -0400

    Spent 2 hours working on writing test for add class gui

--- tests/test_uml_context_gui.py
+++ tests/test_uml_context_gui.py
@@ -1,10 +1,26 @@
 # ScrUML
 # test_uml_context_gui.py
 # Team JJARS
+import pytest
+
 import scruml.uml_context_gui
+from scruml.uml_context_gui import __API
+from scruml.uml_diagram import UMLDiagram
 
 
 def test_activate() -> None:
     # TODO: To be updated with the context_gui module
     # scruml.uml_context_gui.activate()
     pass
+
+
+def test_add_and_remove_class() -> None:
+    api: __API = __API()
+    api.__diagram = UMLDiagram()
+
+    assert api.__diagram.add_class("classA")
+    assert api.__diagram.add_class("classB")
+    assert sorted(api.__diagram.get_all_class_names()) == ["classA", "classB"]
+
+
+# assert api.__diagram.get_all_class_names() == []
