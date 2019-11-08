43af43f2ef121b95ab3bb1b7ec4db7c35e73c9f8
Nick-Russell <Nick-Russell@users.noreply.github.com>
2019-11-08 12:18:42 -0500

    Add testing file for uml_utilities.py

---
+++ tests/test_uml_utilities.py
@@ -0,0 +1,20 @@
+# ScrUML
+# test_uml_utilities.py
+# Team JJARS
+from scruml import uml_utilities
+
+
+def test_parse_class_identifier() -> None:
+    return None
+
+
+def test_parse_relationship_identifier() -> None:
+    return None
+
+
+def test_classify_identifier() -> None:
+    return None
+
+
+def test_stringify_relationship_identifier() -> None:
+    return None
